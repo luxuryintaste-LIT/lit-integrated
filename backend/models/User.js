192a4ad61e7c6d6c367ea83b4466744136cf0c59
JAYADHEEP356 <jayadheep356@gmail.com>
2025-07-17 14:51:27 +0530

    'corrections2'

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Please use a valid email address'],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-}, {
-  timestamps: true,
+  name: String,
+  email: { type: String, unique: true },
+  provider: String, // e.g., "google.com"
+  createdAt: { type: Date, default: Date.now }
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User; 
+module.exports = mongoose.model('User', userSchema);
